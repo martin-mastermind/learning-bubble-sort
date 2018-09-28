1e0a1af72673e9dc744de72f2e01d837b71dfae9
Martin <martbelmoaw@gmail.com>
2018-09-28 17:47:58 +0300

    result

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let arr = [],
+    i = 0,
+		j = 1,
+    start = false;
+
+function setup() {
+  createCanvas(800, 600);
+  
+  for (let i = 0; i < width; i++) {
+    arr.push(random(height));
+  }
+}
+		
+function draw() {
+  background(32, 194, 14);
+  
+  for (let i = 0; i < width; i++) {
+    line(i, height - arr[i], i, height);
+    stroke(0);
+  }
+	
+  if(start){
+  for (let n = 0; n < width / 5; n++) {
+  	if(arr[j - 1] > arr[j]){
+    	swap(arr, j - 1, j);
+  	}
+  	j++;
+  	if(j >= arr.length){
+    	j = 1;
+    	i++;
+  	}
+  }
+  }
+}
+
+function swap(array, i, j) {
+	let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function mousePressed() {
+  start = start ? false : true;
+}
